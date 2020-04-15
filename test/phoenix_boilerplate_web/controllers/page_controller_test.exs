ce4e44fc37e7afc7a30ab3ca968488b09c9c90ee
Jakob Dam Jensen <jakob@nopointlessapps.com>
2020-04-15 21:28:11 +0200

    Test authentication in page controller test

--- test/phoenix_boilerplate_web/controllers/page_controller_test.exs
+++ test/phoenix_boilerplate_web/controllers/page_controller_test.exs
@@ -1,8 +1,26 @@
 defmodule PhoenixBoilerplateWeb.PageControllerTest do
   use PhoenixBoilerplateWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  import PhoenixBoilerplate.Factory
+
+  describe "when authenticated" do
+    setup %{conn: conn} do
+      user = insert(:user)
+      conn = Pow.Plug.assign_current_user(conn, user, otp_app: :phoenix_boilerplate)
+
+      {:ok, conn: conn}
+    end
+
+    test "GET /", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    end
+  end
+
+  describe "when not authenticated" do
+    test "GET /", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 302) =~ "/session/new"
+    end
   end
 end
