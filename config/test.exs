f74838326c582793888aa2b485d3331e0f2e9b78
Jakob Dam Jensen <jakob@nopointlessapps.com>
2020-04-15 19:42:15 +0200

    Initial commit
    Just a plain phoenix application

---
+++ config/test.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# Configure your database
+config :phoenix_boilerplate, PhoenixBoilerplate.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "phoenix_boilerplate_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :phoenix_boilerplate, PhoenixBoilerplateWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
