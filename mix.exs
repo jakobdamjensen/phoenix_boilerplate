a1c0d64702802ffb6e1cd35b04aac764fe9ae8e8
Jakob Dam Jensen <jakob@nopointlessapps.com>
2020-04-29 21:27:40 +0200

    Install oban oban job queue

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule PhoenixBoilerplate.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:oban, "~> 1.2"},
       {:phoenix, "~> 1.4.7"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
